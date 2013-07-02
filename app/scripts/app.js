3a68aa0131738d69c8bba56d5f54dcf26d6a3620
Didier Colens <dcolens@cisco.com>
2013-07-02 17:07:00 +0200

    added angular 1.1.5 and foundation 4

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('EggJogApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
