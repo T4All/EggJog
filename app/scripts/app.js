b17916c877c1e1a3a069cb5c808001e6f9427c21
Didier Colens <dcolens@cisco.com>
2013-07-03 13:19:00 +0200

    bunch of stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,29 @@
 'use strict';
 
 angular.module('EggJogApp', [])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true)
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl',
+        tab: 'home'
       })
-      .otherwise({
-        redirectTo: '/'
+      .when('/stats', {
+        templateUrl: 'views/stats.html',
+        controller: 'StatsCtrl',
+        tab: 'stats'
+      })
+      .when('/trophies', {
+        templateUrl: 'views/trophies.html',
+        controller: 'TrophiesCtrl',
+        tab: 'trophies'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        tab: 'about'
+      })      .otherwise({
+        redirectTo: '/home'
       });
   });
