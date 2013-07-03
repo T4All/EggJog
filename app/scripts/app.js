ffd77fe55a767b3a8758a46a3f0d5502fa9c6c44
Didier Colens <dcolens@cisco.com>
2013-07-03 14:32:34 +0200

    switched to html5mode=flase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 angular.module('EggJogApp', [])
   .config(function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true)
+    $locationProvider.html5Mode(false)
     $routeProvider
       .when('/home', {
         templateUrl: 'views/home.html',
